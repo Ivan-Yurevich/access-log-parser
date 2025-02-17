2d90fa83ebc783c60ef46ffa797ec6a2c85d5f32
Ivan Yurevich <marcusxakep@gmail.com>
2025-02-17 20:51:05 +0300

    Add text length calculator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        System.out.println("Введите текст и нажмите <Enter>:");
+        String text = new Scanner(System.in) .nextLine();
+        System.out.println("Длина текста: " + text.length());
+
+
     }
 }
