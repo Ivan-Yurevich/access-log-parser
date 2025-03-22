e4a0a7acb2d156ea6e37593a572ec7a08d15b9cc
Ivan Yurevich <marcusxakep@gmail.com>
2025-03-22 20:57:52 +0300

    Add file checker

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+            public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int validFileCount = 0;
+            while (true) {
+                System.out.print("Введите путь к файлу: ");
+                String filePath = scanner.nextLine();
+                File file = new File("C:/Users/IYurevich/File.txt");
+                boolean fileExists = file.exists();
+                boolean isFile = file.isFile();
+                if (!fileExists || !isFile) {
+                    System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу.");
+                    continue;
+                } else {
+                    validFileCount++;
+                    System.out.println("Путь указан верно. Это файл номер " + validFileCount);
+                }
+            }
+        }
+
+
     }
-}
+
